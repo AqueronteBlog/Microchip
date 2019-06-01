498225bb2505caecd1b8c0d0f110a9f013f98a85
Manuel Caballero <aqueronteblog@gmail.com>
2019-06-01 21:20:19 +0100

    PIC32MM0256GPM064 Timer1 Example. Interrupts files were updated

---
+++ XC32/Examples/Timer1.X/src/interrupts.c
@@ -0,0 +1,13 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        01/June/2019
+ * @version     01/June/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "../inc/interrupts.h"
