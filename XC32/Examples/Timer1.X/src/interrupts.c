7fd74f5519140fd48be24e127f9d5943e6481f7e
Manuel Caballero <aqueronteblog@gmail.com>
2019-06-02 21:46:15 +0100

    PIC32MM0256GPM064 Timer1 Example. Timer1 subroutine was added, it is still in progress

--- XC32/Examples/Timer1.X/src/interrupts.c
+++ XC32/Examples/Timer1.X/src/interrupts.c
@@ -11,3 +11,24 @@
  * @warning     N/A
  */
 #include "../inc/interrupts.h"
+
+
+/**
+ * @brief       void T1Interrupt ()
+ * @details     Timer interruption. 
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        02/June/2019
+ * @version     02/June/2019   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A
+ */
+void __attribute__ ( ( vector(_TIMER_1_VECTOR) , interrupt(IPL3SOFT) ) ) T1Interrupt ( void )
+{
+    
+    /* Clear the timer interrupt status flag ( T1IF )     */
+    IFS0SET  =   0x00020000;
+}
