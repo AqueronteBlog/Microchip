f07966dd547668a2e978d32593d9afd840112772
Manuel Caballero <aqueronteblog@gmail.com>
2021-11-30 15:41:40 +0000

    PIC32MX470 LED Example. Interrupts files were added.

---
+++ XC32/PIC32MX470F512H/Examples/LED/LED.X/inc/interrupts.h
@@ -0,0 +1,44 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        19/May/2019
+ * @version     19/May/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "board.h"
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Subroutine prototypes.
+ */
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
