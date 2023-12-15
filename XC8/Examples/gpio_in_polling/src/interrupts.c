1e418d9203499d3658a5d21430e6899dd5c45841
Manuel Caballero <aqueronteblog@gmail.com>
2023-12-15 20:07:09 +0000

    Example. GPIO In Polling. Functions files were added
    This project is still in progress

---
+++ XC8/Examples/gpio_in_polling/src/interrupts.c
@@ -0,0 +1,13 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interrupts sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        15/December/2023
+ * @version     15/December/2023    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "../inc/interrupts.h"
