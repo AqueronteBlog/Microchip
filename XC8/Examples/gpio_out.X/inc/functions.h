d97b2882899567e721d350fda208f7fcf04de94a
Manuel Caballero <aqueronteblog@gmail.com>
2023-12-15 15:12:33 +0000

    Example. GPIO Out. Functions files were added

---
+++ XC8/Examples/gpio_out.X/inc/functions.h
@@ -0,0 +1,42 @@
+/**
+ * @brief       functions.h
+ * @details     Functions header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        08/12/2023
+ * @version     08/12/2023   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include "../inc/board.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Function prototypes.
+ */
+void conf_GPIO  ( void );
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTIONS_H_ */
---
+++ XC8/Examples/gpio_out.X/src/functions.c
@@ -0,0 +1,50 @@
+/**
+ * @brief       functions.c
+ * @details     Functions sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        08/December/2023
+ * @version     08/December/2023   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "../inc/functions.h"
+
+
+/**
+ * @brief       void conf_GPIO ( void )
+ * @details     It configures GPIOs.
+ * 
+ *              PORTB
+ *                  - RB0: GPIO Output pin, no pull-up
+ *                  - RB1: GPIO Output pin, no pull-up
+ *                  - RB2: GPIO Output pin, no pull-up
+ *                  - RB3: GPIO Output pin, no pull-up
+ * 
+ *
+ * @param[in]    N/A.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        08/December/2023
+ * @version     08/December/2023    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void conf_GPIO ( void )
+{
+    /* RB0, RB1, RB2 and RB3 as digital output pins */
+    ANSELB  &=  ~( D2 | D3 | D4 | D5 );
+    
+    /* RB0, RB1, RB2 and RB3 as output pins */
+    TRISB   &=  ~( D2 | D3 | D4 | D5 );
+    
+    /* RB0, RB1, RB2 and RB3 no pull-ups */
+    WPUB    &=  ~( D2 | D3 | D4 | D5 );
+}
