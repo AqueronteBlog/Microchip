0839717c9075a9325fbed1e26a66da2f1bc90999
Manuel Caballero <aqueronteblog@gmail.com>
2023-12-15 20:06:52 +0000

    Example. GPIO In Polling. Header files were added
    This project is still in progress

---
+++ XC8/Examples/gpio_in_polling/inc/board.h
@@ -0,0 +1,44 @@
+/**
+ * @brief       board.h
+ * @details     Board header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        15/December/2023
+ * @version     15/December/2023    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#ifndef BOARD_H_
+#define BOARD_H_
+
+#include <xc.h>
+#include <pic16f1937.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Function prototypes.
+ */
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTIONS_H_ */
---
+++ XC8/Examples/gpio_in_polling/inc/functions.h
@@ -0,0 +1,41 @@
+/**
+ * @brief       functions.h
+ * @details     Functions header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        15/December/2023
+ * @version     15/December/2023    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include "board.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Function prototypes.
+ */
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTIONS_H_ */
