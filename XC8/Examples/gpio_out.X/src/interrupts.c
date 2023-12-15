22e1700b77ea7d143029953812f17f3245bdaa9b
Manuel Caballero <aqueronteblog@gmail.com>
2023-12-15 15:12:16 +0000

    Example. GPIO Out. Interrupt files were added

---
+++ XC8/Examples/gpio_out.X/src/interrupts.c
@@ -0,0 +1,13 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interrupts sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        08/December/2023
+ * @version     08/December/2023   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "../inc/interrupts.h"
