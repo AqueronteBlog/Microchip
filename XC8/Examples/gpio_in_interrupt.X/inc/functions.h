6210634263bf467a257159e24aa7cb13f1b5404f
Manuel Caballero <aqueronteblog@gmail.com>
2024-01-30 16:04:38 +0000

    Example. GPIO In Interrupt. Functions files were added

---
+++ XC8/Examples/gpio_in_interrupt.X/inc/functions.h
@@ -0,0 +1,41 @@
+/**
+ * @brief       functions.h
+ * @details     Functions header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        30/January/2024
+ * @version     30/January/2024    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include "board.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Function prototypes.
+ */
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTIONS_H_ */
---
+++ XC8/Examples/gpio_in_interrupt.X/src/functions.c
@@ -0,0 +1,13 @@
+/**
+ * @brief       functions.c
+ * @details     Functions sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        30/January/2024
+ * @version     30/January/2024    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "../inc/functions.h"
