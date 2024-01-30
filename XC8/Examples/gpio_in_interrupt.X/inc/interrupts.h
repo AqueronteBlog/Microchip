fdcea921a2ee7f82de0d6c8b4becc6a8f75ae560
Manuel Caballero <aqueronteblog@gmail.com>
2024-01-30 16:04:51 +0000

    Example. GPIO In Interrupt. Interrupts files were added

---
+++ XC8/Examples/gpio_in_interrupt.X/inc/interrupts.h
@@ -0,0 +1,42 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        30/January/2024
+ * @version     30/January/2024    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#include "board.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Subroutine prototypes.
+ */
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
---
+++ XC8/Examples/gpio_in_interrupt.X/src/interrupts.c
@@ -0,0 +1,13 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interrupts sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        30/January/2024
+ * @version     30/January/2024    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "../inc/interrupts.h"
