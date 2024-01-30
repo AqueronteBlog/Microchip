0611e4571137a25f9588d09f9435cd3a75c03089
Manuel Caballero <aqueronteblog@gmail.com>
2024-01-30 16:05:40 +0000

    Example. GPIO In Interrupt. This project has just started, it is still in progress
    Last compilation without any errors

---
+++ XC8/Examples/gpio_in_interrupt.X/src/main.c
@@ -0,0 +1,35 @@
+/**
+ * @brief       main.c
+ * @details     [TODO]
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        30/January/2024
+ * @version     30/January/2024    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ * @pre         N/A
+ */
+#include "../inc/board.h"
+#include "../inc/functions.h"
+#include "../inc/interrupts.h"
+
+/**@brief Constants.
+ */
+
+
+/**@brief Variables.
+ */
+
+
+/**@brief Function for application main entry.
+ */
+void main(void) {
+    
+    while ( 1U )
+    {
+        
+    }
+}
