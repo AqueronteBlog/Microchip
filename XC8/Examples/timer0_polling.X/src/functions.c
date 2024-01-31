b6a2a475bd906dea38008155e6bba3eaab4e039f
Manuel Caballero <aqueronteblog@gmail.com>
2024-01-31 17:12:21 +0000

    Example. Timer0 polling. Functions files were added

---
+++ XC8/Examples/timer0_polling.X/src/functions.c
@@ -0,0 +1,13 @@
+/**
+ * @brief       functions.c
+ * @details     Functions sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        31/January/2024
+ * @version     31/January/2024    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "../inc/functions.h"
