727912af9b8652dff9f213dcc81229173a55a5e4
Manuel Caballero <aqueronteblog@gmail.com>
2024-01-31 17:12:34 +0000

    Example. Timer0 polling. Interrupts files were added

---
+++ XC8/Examples/timer0_polling.X/src/interrupts.c
@@ -0,0 +1,13 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interrupts sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        31/January/2024
+ * @version     31/January/2024    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "../inc/interrupts.h"
