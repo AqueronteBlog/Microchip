30ddfe65111e31632aa5f05b921ffd73a2ab0f10
Manuel Caballero <aqueronteblog@gmail.com>
2024-02-09 12:13:12 +0000

    Example. Timer2/4/6 interrupt. Interrupts files were added

---
+++ XC8/Examples/timer2_4_6_interrupt.X/src/interrupts.c
@@ -0,0 +1,43 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interrupts sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        09/February/2024
+ * @version     09/February/2024    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "../inc/interrupts.h"
+
+
+/**
+ * @brief       void ISR ()
+ * @details     Interrupt subroutine. 
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        09/February/2024
+ * @version     09/February/2024   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A
+ */
+void __interrupt() ISR ( void )
+{
+    /* Check if Timer0 Overflow interrupt is enabled and Timer0 Overflow occurred */
+    if ( ( INTCONbits.TMR0IE == 1U  ) && ( INTCONbits.TMR0IF == 1U ) )
+    {        
+        /* Update the variable  */
+        myState =   1U;
+        
+        /* Reload Timer0, it overflows every 0.5s  */
+        TMR0    =   195U;
+        
+        /* Clear the interrupt flag   */
+        INTCONbits.TMR0IF = 0U;
+    }
+}
