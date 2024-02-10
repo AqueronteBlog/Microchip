f414569bf43ea81419f4b23b318e16c4a952061e
Manuel Caballero <aqueronteblog@gmail.com>
2024-02-10 10:51:56 +0000

    Example. EUSART Asynchronous.  Interrupt files were added.
    They are still in progress

---
+++ XC8/Examples/eusart_asynchronous.X/src/interrupts.c
@@ -0,0 +1,32 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interrupts sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        10/February/2024
+ * @version     10/February/2024    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "../inc/interrupts.h"
+
+
+/**
+ * @brief       void ISR ()
+ * @details     Interrupt subroutine. 
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        09/February/2024
+ * @version     09/February/2024   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A
+ */
+void __interrupt() ISR ( void )
+{
+
+}
