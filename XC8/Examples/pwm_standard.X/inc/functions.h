7acbe80f8fbefea8f234df6709e866a5d38f4636
Manuel Caballero <aqueronteblog@gmail.com>
2024-02-13 15:41:25 +0000

    Example. PWM standard. Functions files were added

---
+++ XC8/Examples/pwm_standard.X/inc/functions.h
@@ -0,0 +1,45 @@
+/**
+ * @brief       functions.h
+ * @details     Functions header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero (aqueronteblog@gmail.com)
+ * @date        13/February/2024
+ * @version     13/February/2024    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include "board.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Function prototypes.
+ */
+void conf_CLK       ( void );
+void conf_GPIO      ( void );
+void conf_Timer2    ( void );
+void conf_Timer4    ( void );
+void conf_Timer6    ( void );
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTIONS_H_ */
